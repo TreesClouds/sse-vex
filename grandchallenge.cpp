30e8ae813b6d6ec8433687d088b1db1711f1c510
Alexander DeRouen <34390214+TreesClouds@users.noreply.github.com>
2022-01-17 17:44:44 -0800

    condensation, organization

--- grandchallenge.cpp
+++ grandchallenge.cpp
@@ -10,10 +10,11 @@
 // ---- START VEXCODE CONFIGURED DEVICES ----
 // Robot Configuration:
 // [Name]               [Type]        [Port(s)]
-// backDistance         distance      11              
-// Vision               vision        20              
-// frontDistance        distance      12              
 // Drivetrain           drivetrain    1, 2, 10, 3, 9  
+// frontDistance        distance      12              
+// backDistance         distance      11              
+// Vision20             vision        20              
+// Bumper               bumper        A               
 // ---- END VEXCODE CONFIGURED DEVICES ----
 
 #include "vex.h"
@@ -22,15 +23,28 @@
 using namespace vex;
 
 /**
- * Sticks to a right-side wall until the wall is no longer detected
- * Function ends with robot facing right compared to starting position
+ * Sticks to a wall until the wall is no longer detected
+ * Function ends with robot turned around the wall's corner
  * 
  * @param inchesBeforeCheck Inches to travel before final wall clearance confirmation 
+ * @param wallDirection Direction of wall to follow relative to destination
  */
-void stickToWall(int inchesBeforeCheck) {
+void stickToWall(int inchesBeforeCheck, turnType wallDirection) {
 
-  Drivetrain.setDriveVelocity(40, percent);
-  Drivetrain.setTurnVelocity(4, percent);
+  turnType wallOpposite;
+  directionType driveDirection;
+  directionType driveOpposite;
+
+  // Converts direction of wall to other corresponding variables
+  if (wallDirection == right) {
+    driveDirection = forward;
+    driveOpposite = reverse;
+    wallOpposite = left;
+  } else {
+    driveDirection = reverse;
+    driveOpposite = forward;
+    wallOpposite = right;
+  }
 
   while(true) {
 
@@ -45,27 +59,27 @@ void stickToWall(int inchesBeforeCheck) {
       // If the robot strays too far from the wall, get closer to the wall
       if (frontDistance.objectDistance(inches) > 40 || backDistance.objectDistance(inches) > 40) {
         
-        Drivetrain.turnFor(right, 45, degrees);
-        Drivetrain.driveFor(forward, 600, mm);
-        Drivetrain.turnFor(left, 45, degrees);
+        Drivetrain.turnFor(wallDirection, 45, degrees);
+        Drivetrain.driveFor(driveDirection, 600, mm);
+        Drivetrain.turnFor(wallOpposite, 45, degrees);
         
       } else {
         
         // Turns towards wall if turned away from wall
         while(frontDistance.objectDistance(inches) > backDistance.objectDistance(inches)) {
-          Drivetrain.turn(right);
+          Drivetrain.turn(wallDirection);
         }
         Drivetrain.stop();
 
         // Turns away from wall if turned towards wall
         while(frontDistance.objectDistance(inches) < backDistance.objectDistance(inches)) {
-          Drivetrain.turn(left);
+          Drivetrain.turn(wallOpposite);
         }
         Drivetrain.stop();
 
         // Drives forward after realigning with wall
         wait(100, msec);
-        Drivetrain.driveFor(forward, 400, mm);
+        Drivetrain.driveFor(driveDirection, 400, mm);
         wait(100, msec);
         
       }
@@ -74,10 +88,10 @@ void stickToWall(int inchesBeforeCheck) {
     Drivetrain.stop(); // Potentially redunant line, further testing needed
     
     // Wall Clearance Check #2 (moves forward slightly and rechecks once to confirm lack of anomalies in rapid data)
-    Drivetrain.driveFor(forward, 100, mm);
+    Drivetrain.driveFor(driveDirection, 100, mm);
     if (frontDistance.objectDistance(inches) == 0 && backDistance.objectDistance(inches) == 0) {
       
-      Drivetrain.driveFor(forward, inchesBeforeCheck, inches);
+      Drivetrain.driveFor(driveDirection, inchesBeforeCheck, inches);
       Drivetrain.turnFor(right, 90, degrees);
       
       // Wall Clearance Check #3 (physically checks for wall using bumper sensor to confirm both other checks)
@@ -89,85 +103,7 @@ void stickToWall(int inchesBeforeCheck) {
           Drivetrain.stop();
           Drivetrain.driveFor(reverse, 20, inches);
           Drivetrain.turnFor(left, 90, degrees);
-          stickToWall(inchesBeforeCheck);
-        }
-        
-      }
-      
-      break; // End function if bumper sensor does not detect wall
-      
-    }
-  }
-} 
-
-/**
- * Sticks to a left-side wall until the wall is no longer detected
- * Function ends with robot facing right compared to starting position
- * 
- * @param inchesBeforeCheck Inches to travel before final wall clearance confirmation 
- */
-void stickBackwards(int inchesBeforeCheck) {
-
-  Drivetrain.setDriveVelocity(40, percent);
-  Drivetrain.setTurnVelocity(4, percent);
-
-  while(true) {
-
-    // Wall Clearance Check #1 (rapidly checked distance sensor data)
-    while (frontDistance.objectDistance(inches) != 0 && backDistance.objectDistance(inches) != 0) {
-
-      // Logs distance sensor data to computer console for remote debugging purposes
-      std::cout << frontDistance.objectDistance(inches) << std::endl;
-      std::cout << backDistance.objectDistance(inches) << std::endl;
-      std::cout << "---" << std::endl;
-
-      // If the robot strays too far from the wall, get closer to the wall
-      if (frontDistance.objectDistance(inches) > 40 || backDistance.objectDistance(inches) > 40) {
-        
-        Drivetrain.turnFor(left, 45, degrees);
-        Drivetrain.driveFor(reverse, 600, mm);
-        Drivetrain.turnFor(right, 45, degrees);
-        
-      } else {
-        
-        // Turns away from wall if turned towards wall
-        while(frontDistance.objectDistance(inches) < backDistance.objectDistance(inches)) {
-          Drivetrain.turn(left);
-        }
-        Drivetrain.stop();
-
-        // Turns towards from wall if turned away from wall
-        while(frontDistance.objectDistance(inches) > backDistance.objectDistance(inches)) {
-          Drivetrain.turn(right);
-        }
-        Drivetrain.stop();
-
-        // Drives reverse after realigning with wall
-        wait(100, msec);
-        Drivetrain.driveFor(reverse, 400, mm);
-        wait(100, msec);
-      }
-    }
-
-    Drivetrain.stop(); // Potentially redunant line, further testing needed
-    
-    // Wall Clearance Check #2 (moves forward slightly and rechecks once to confirm lack of anomalies in rapid data)
-    Drivetrain.driveFor(reverse, 100, mm);
-    if (frontDistance.objectDistance(inches) == 0 && backDistance.objectDistance(inches) == 0) {
-      
-      Drivetrain.driveFor(reverse, inchesBeforeCheck, inches);
-      Drivetrain.turnFor(right, 90, degrees);
-      
-      // Wall Clearance Check #3 (physically checks for wall using bumper sensor to confirm both other checks)
-      Drivetrain.driveFor(forward, 45, inches, false);
-      while(Drivetrain.isMoving()) {
-        
-        // If wall detected physically, move away from wall and restart function with the same parameters
-        if (Bumper.pressing()) {
-          Drivetrain.stop();
-          Drivetrain.driveFor(reverse, 20, inches);
-          Drivetrain.turnFor(left, 90, degrees);
-          stickBackwards(inchesBeforeCheck);
+          stickToWall(inchesBeforeCheck, wallDirection);
         }
         
       }
@@ -200,31 +136,33 @@ int main() {
   vexcodeInit();
   std::cout << "PROGRAM START" << std::endl;
   Drivetrain.setRotation(0, degrees);
+  Drivetrain.setDriveVelocity(40, percent);
+  Drivetrain.setTurnVelocity(4, percent);
   wait(1, seconds);
 
   // Phase 1: To Ramp
   Drivetrain.driveFor(forward, 36, inches);     // Drive out of classroom
   Drivetrain.turnFor(right, 85, degrees);       // Turn away from classroom
   Drivetrain.driveFor(forward, 282, inches);    // Drive across robot yard gate
-  stickToWall(50);                              // Drive across 400 classroom wall, turn towards parking lot ramp
+  stickToWall(50, right);                       // Drive across 400 classroom wall, turn towards parking lot ramp
   Drivetrain.driveFor(forward, 144, inches);    // Drive across unwalled portion of parking lot ramp
-  stickToWall(50);                              // Drive across walled portion of parking lot ramp, turn towards back of 400 classrooms
+  stickToWall(50, right);                       // Drive across walled portion of parking lot ramp, turn towards back of 400 classrooms
   Drivetrain.driveFor(forward, 192, inches);    // Drive across unwalled space towards ramp entrance
   Drivetrain.turnFor(right, 180, degrees);      // Face distance sensors towards left-side wall
-  stickBackwards(30);                           // Drive across back of 400 classrooms, turn towards ramp entrance
+  stickToWall(30, left);                        // Drive across back of 400 classrooms, turn towards ramp entrance
   
   // Phase 2: Ramp
   driveUntilBump(12);                           // Drive until first ramp wall detected
   Drivetrain.turnFor(left, 90, degrees);        // Face distance sensors towards left-side wall
-  stickBackwards(30);                           // Drive across first (west-facing) portion of ramp
+  stickToWall(30, left);                        // Drive across first (west-facing) portion of ramp
   Drivetrain.driveFor(forward, 36, inches);     // Drive from end of first to start of second portion of ramp
   Drivetrain.turnFor(left, 90, degrees);        // Face distance sensors towards right-side wall
-  stickToWall(30);                              // Drive across second (east-facing) portion of ramp, turn towards hallway
+  stickToWall(30, right);                       // Drive across second (east-facing) portion of ramp, turn towards hallway
   Drivetrain.driveFor(forward, 216, inches);    // Drive into the hallway entrance
   
   // Phase 3: 200 Hall, 300 Hall
-  stickToWall(50);                              // Drive across east-side hallway
-  stickToWall(20);                              // Drive across 300 hallway, turn away from inside ramp
+  stickToWall(50, right);                       // Drive across east-side hallway
+  stickToWall(20, right);                       // Drive across 300 hallway, turn away from inside ramp
   Drivetrain.driveFor(reverse, 216, inches);    // Drive across unwalled space onto the inside ramp
-  stickBackwards(50);                           // Drive across inside ramp
+  stickToWall(50, left);                        // Drive across inside ramp
 }
